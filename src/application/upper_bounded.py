990733854548d8e2e68f3495ac9691abd2fb614e
Abega1642 <hei.abega.2@gmail.com>
2025-01-26 16:37:45 +0300

    feat: implement first statement

---
+++ src/application/upper_bounded.py
@@ -0,0 +1,15 @@
+import math
+import src.functions.sigma as sigma
+import src.functions.totient as totient
+
+def number_of_n_such_as_phi_of_sigma_of_n_leq_than_cn(c, x):
+    res = 0
+    values = []
+    for n in range(1, math.floor(x) + 1):
+        if totient.phi(sigma.sigma(n)) <= c*n :
+            res +=1
+            values.append([n, totient.phi(sigma.sigma(n))])
+
+    return res
+
+print(number_of_n_such_as_phi_of_sigma_of_n_leq_than_cn(5, 9))
